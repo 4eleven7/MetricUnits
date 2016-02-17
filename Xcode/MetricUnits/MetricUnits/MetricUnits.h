cb5ff71e1e81b074b63337f7405073acdb2d00c1
Dan Love <dan@daniellove.net>
2016-02-17 22:24:17 +0000

    HARNESS: Added test harness for unit tests

---
+++ Xcode/MetricUnits/MetricUnits/MetricUnits.h
@@ -0,0 +1,19 @@
+//
+//  MetricUnits.h
+//  MetricUnits
+//
+//  Created by Daniel Love on 16/02/2016.
+//  Copyright © 2016 Daniel Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MetricUnits.
+FOUNDATION_EXPORT double MetricUnitsVersionNumber;
+
+//! Project version string for MetricUnits.
+FOUNDATION_EXPORT const unsigned char MetricUnitsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MetricUnits/PublicHeader.h>
+
+
