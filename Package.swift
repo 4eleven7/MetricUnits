5464fc8eca55d162f632270668c346a97c6c67bd
Dan Love <dan@daniellove.net>
2016-02-17 22:25:53 +0000

    PACKAGE: Added package file for Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,11 @@
+/*
+  Created by Daniel Love on 16/02/2016.
+  Copyright © 2016 Daniel Love and Shh Love Limited. All rights reserved.
+*/
+
+import PackageDescription
+
+let package = Package(
+	name: "MetricUnits",
+	exclude: ["Xcode", "fastlane"]
+)
